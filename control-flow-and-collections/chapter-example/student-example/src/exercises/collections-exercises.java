d0fed5681c459d619913df92ad17a541bf94d2bc
Jenna Tan <9787333+jenna215@users.noreply.github.com>
2023-08-21 21:05:16 -0400

    circle

---
+++ control-flow-and-collections/chapter-example/student-example/src/exercises/collections-exercises.java
@@ -0,0 +1,9 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class collections-exercises {
+    public static void main(String[] args) {
+        int[] values = new int[1, 1, 2, 3, 5, 8];
+
+        }
