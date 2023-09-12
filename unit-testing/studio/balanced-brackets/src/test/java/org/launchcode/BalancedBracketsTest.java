e51bf2635c0e03e1d66f50c0f38e067e81f5be5d
Jenna Tan <9787333+jenna215@users.noreply.github.com>
2023-09-11 21:10:19 -0400

    chapter 6 studio

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -5,8 +5,55 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BalancedBracketsTest {
-    //TODO: add tests here
     @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+        assertnotNull(BalancedBrackets.hasBalancedBrackets("[]"));
+        assertEquals(BalancedBrackets.hasBalancedBrackets("[]", "[]"));
+    }
+
+    public void oneBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    public void noBrackets() {
+        String bracketless = "Nope";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(bracketless));
+    }
+
+    public void inTheMiddle() {
+        String test = "Ban[ana";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(test));
+    }
+
+    public void atTheEnd() {
+        String test = "Banana]";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(test));
+    }
+
+    public void backwardsBrackets() {
+        String test = "Ban][ana";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(test));
+    }
+
+    public void inTheMiddle() {
+        String test = "Ban[ana";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(test));
+    }
+
+    @Test
+    public void unbalancedInString() {
+        String launch = "Launch]Code[";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(launch));
+    }
+
+    public void unbalanced() {
+        String unbalancedString = "][";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(unbalancedString));
+    }
+
     public void emptyTest() {
         assertEquals(true, true);
     }
